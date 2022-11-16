27dec07a8f875e61b7f35e9a2811825ad5a9e28f
rkdalsrbs0 <rkdalsrbs0@naver.com>
2022-11-17 01:27:45 +0900

    수정

--- greprs/src/main.rs
+++ greprs/src/main.rs
@@ -77,23 +77,6 @@ struct Config {
     filename: String,
 }
 
-fn parse_config2(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
-
-    Config { query, filename }
-}
-
-///: 세번째 리팩토링
-fn main3() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::new(&args);
-
-    // ...snip...
-}
-
-// ...snip...
 
 impl Config {
     fn new(args: &[String]) -> Config {
@@ -117,4 +100,49 @@ impl Config {
 
         Ok(Config { query, filename })
     }
+}
+
+fn parse_config2(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename }
+}
+
+///: 세번째 리팩토링
+fn main3() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args);
+
+    // ...snip...
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    run(config);
+}
+
+fn run(config: Config) {
+    let mut f = File::open(config.filename).expect("file not found");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents).expect("something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
+}
+// ...snip...
+
+use std::error::Error;
+
+// ...snip...
+
+fn run2(config: Config) -> Result<(), Box<dyn Error>> {
+    let mut f = File::open(config.filename)?;
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)?;
+
+    println!("With text:\n{}", contents);
+
+    Ok(())
 }
